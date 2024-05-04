fc7b6557f3da7f4065c55ee0b198c2926523b1c8
hann <wjdgks1233@gmail.com>
2024-05-04 16:48:48 +0900

    first commit

---
+++ src/main/java/com/example/searchProject/SearchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchProjectApplication.class, args);
+	}
+
+}
